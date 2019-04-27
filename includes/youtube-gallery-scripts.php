d4d07cf82c6ac5ba0e462609cd38d1f15579b73c
acbrent25 <adamchampagne@gmail.com>
2019-04-27 11:09:50 -0400

    create custom video post type

--- includes/youtube-gallery-scripts.php
+++ includes/youtube-gallery-scripts.php
@@ -1,6 +1,18 @@
 <?php
 
-// Add Scripts
+// Check if admin and load amdin scripts
+
+if ( is_admin()){
+    function yg_add_admin_scripts(){
+        wp_enqueue_style('yg-main-admin-style', plugins_url() . '/youtube-gallery/css/admin-style.css');
+        wp_enqueue_script('yg-main-script', plugins_url() . '/youtube-gallery/js/main.js');
+
+    }
+
+    add_action('admin-init', 'yg_add_admin_scripts');
+}
+
+
 
 function yg_add_scripts(){
     wp_enqueue_style('yg-main-style', plugins_url() . '/youtube-gallery/css/style.css');
